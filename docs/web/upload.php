d23191a12d56c696747eef63b569cac3eba82df7
Sana Client Devs <84273995+SanaDevelopers@users.noreply.github.com>
2021-07-22 18:47:15 -0400

    Update upload.php

--- docs/web/upload.php
+++ docs/web/upload.php
@@ -3,7 +3,6 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -12,20 +11,7 @@ if(isset($_POST["submit"])) {
     $uploadOk = 1;
   } else {
     echo "File is not an image.";
-    $uploadOk = 1;
+    $uploadOk = 0;
   }
 }
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 5000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
 ?>
